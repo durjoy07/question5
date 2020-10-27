ef58cca1ae03f2fc7c4096332de8003d9d6fd868
durjoy07 <37486359+durjoy07@users.noreply.github.com>
2020-10-28 02:02:30 +0600

    Update Category.jsx

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -33,7 +33,6 @@ const MenuProps = {
 
 const Category = () => {
   const classes = useStyles();
-  const theme = useTheme();
   const [open, setOpen] = useState(false);
   const [createOpen, setCreateOpen] = useState(false);
   const [catogerys, setCatogerys] = useState([
